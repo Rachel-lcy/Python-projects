7584f34aacf6f4f286d3e34e0fb4559b25e0162b
Rachel <hurui.lcy@gmail.com>
2025-05-05 19:28:00 -0600

    functions return

--- ex_03_03.py
+++ ex_03_03.py
@@ -19,13 +19,13 @@ try:
         
     elif s >= 0.8:
         print("B")
-        
+      
     elif s >= 0.7:
         print("C")
-        
+
     elif s >= 0.6:
         print("D")
-        
+
     else:
         print("F")
 
