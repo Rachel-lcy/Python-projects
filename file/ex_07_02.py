f90c36b54947af93d05f287a5c2c5af17bceaacb
Rachel <hurui.lcy@gmail.com>
2025-05-09 22:12:03 -0600

    open and read files

--- file/ex_07_02.py
+++ file/ex_07_02.py
@@ -1,5 +1,5 @@
 #Exercise 1: Write a program to read through a file and print the contents of the file (line by line) all in upper case. Executing the program will look as follow
-fh = open('file/mbox-short.txt')
+fh = open('mbox-short.txt')
 
 for line in fh:
   line = line.rstrip()
