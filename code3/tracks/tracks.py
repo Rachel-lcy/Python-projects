ff1856800950bfcfde0936c3e801986920db9a8e
Rachel <hurui.lcy@gmail.com>
2025-05-27 18:06:54 -0600

    connect with SQlite database

--- code3/tracks/tracks.py
+++ code3/tracks/tracks.py
@@ -47,19 +47,19 @@ for line in handle:
 
     print(name, artist, album, count, rating, length)
 
-    cur.execute('''INSERT OR IGNORE INTO Artist (name) 
+    cur.execute('''INSERT OR IGNORE INTO Artist (name)
         VALUES ( ? )''', ( artist, ) )
     cur.execute('SELECT id FROM Artist WHERE name = ? ', (artist, ))
     artist_id = cur.fetchone()[0]
 
-    cur.execute('''INSERT OR IGNORE INTO Album (title, artist_id) 
+    cur.execute('''INSERT OR IGNORE INTO Album (title, artist_id)
         VALUES ( ?, ? )''', ( album, artist_id ) )
     cur.execute('SELECT id FROM Album WHERE title = ? ', (album, ))
     album_id = cur.fetchone()[0]
 
     cur.execute('''INSERT OR REPLACE INTO Track
-        (title, album_id, len, rating, count) 
-        VALUES ( ?, ?, ?, ?, ? )''', 
+        (title, album_id, len, rating, count)
+        VALUES ( ?, ?, ?, ?, ? )''',
         ( name, album_id, length, rating, count ) )
 
     conn.commit()
